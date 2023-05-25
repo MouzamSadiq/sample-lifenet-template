931cca9db65de214c2f20b5b996a1b5136a3b89b
MouzamSadiq <mouzam@systalent>
2023-05-25 23:39:34 +0300

    ui chnages

--- src/pages/Home/Konva/Konva.tsx
+++ src/pages/Home/Konva/Konva.tsx
@@ -6,14 +6,27 @@ const KonvaGround = () => {
   const stageReference = useRef(null);
   const toolbarArrowReference = useRef(null);
   const [rects, setRects] = useState([{}]);
+  const [stageWidth, setStageWidth] = useState(window.innerWidth);
+  const [stageHeight, setStageHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setStageWidth(window.innerWidth);
+      setStageHeight(window.innerHeight);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   // ------------ Image Rendering Function---------------
   const URLImage = ({ src }: URLImageProps) => {
     // Calculate the position to center the image
-    const stageWidth = window.innerWidth;
-    const stageHeight = window.innerHeight;
-    const width = stageWidth / 2.5;
-    const height = stageHeight / 8;
+    const width = (stageWidth - 355) / 2;
+    const height = stageHeight / 4.5;
     const [image, setImage] = useState<HTMLImageElement | undefined>();
     useEffect(() => {
       const img = new window.Image();
@@ -47,20 +60,31 @@ const KonvaGround = () => {
   };
 
   const ToolBar = () => {
+    const rectWidth = 555;
+    const rectHeight = 77.5;
+    const rectX = (stageWidth - rectWidth) / 2;
+    const rectY = 5;
+    const handleMouseEnter = () => {
+      document.body.style.cursor = "pointer";
+    };
+
+    const handleMouseLeave = () => {
+      document.body.style.cursor = "default";
+    };
     return (
       <>
         <Text
           fontSize={30}
           text="Toolbar"
           fontFamily="Belgrano"
-          x={15}
-          y={55}
+          x={rectWidth / 2}
+          y={rectHeight / 2}
         />
         <Rect
-          y={15}
-          x={510}
+          x={rectX}
+          y={rectY}
           width={555}
-          height={77.5}
+          height={rectHeight}
           fill="white"
           shadowBlur={5}
           shadowColor="black"
@@ -68,41 +92,52 @@ const KonvaGround = () => {
 
         <Arrow
           id="toolbarArrow"
-          points={[25, 255, 80, 255]}
+          points={[
+            rectX + rectWidth / 6,
+            rectY + rectHeight / 2,
+            rectX + rectWidth / 3,
+            rectY + rectHeight / 2,
+          ]}
+          // points={[25, 255, 80, 255]}
           fill="black"
           stroke="black"
           ref={toolbarArrowReference}
           draggable={true}
           onDragEnd={handleDragEnds}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
         />
       </>
     );
   };
+
   return (
-    <Stage
-      width={window.innerWidth}
-      height={window.innerHeight}
-      ref={stageReference}
-    >
-      <Layer>
-        <URLImage src="https://lifenethealth.visualstudio.com/8f99a695-9545-4cb5-a249-dbfe0d365a3f/_apis/git/repositories/92dd74a8-03df-4361-aaa0-32fe4091992f/items?path=/ProcessingLogs/ProcessingLogs_iOS/ProcessingLogs_Aortoilliac/Resources/AortoIliacArteryDiagram.png&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=octetStream&api-version=5.0" />
-      </Layer>
-      <Layer>
-        {rects.map((eachRect: any) => (
-          <Arrow
-            id="draggedArrowId"
-            points={[eachRect.x - 100, eachRect.y, eachRect.x, eachRect.y]}
-            fill="black"
-            stroke="black"
-            draggable={eachRect.draggable}
-            key={eachRect.x + eachRect.y}
-          />
-        ))}
-      </Layer>
-      <Layer>
-        <ToolBar />
-      </Layer>
-    </Stage>
+    <div>
+      <Stage
+        width={window.innerWidth}
+        height={window.innerHeight}
+        ref={stageReference}
+      >
+        <Layer>
+          <URLImage src="https://lifenethealth.visualstudio.com/8f99a695-9545-4cb5-a249-dbfe0d365a3f/_apis/git/repositories/92dd74a8-03df-4361-aaa0-32fe4091992f/items?path=/ProcessingLogs/ProcessingLogs_iOS/ProcessingLogs_Aortoilliac/Resources/AortoIliacArteryDiagram.png&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=octetStream&api-version=5.0" />
+        </Layer>
+        <Layer>
+          {rects.map((eachRect: any) => (
+            <Arrow
+              id="draggedArrowId"
+              points={[eachRect.x - 100, eachRect.y, eachRect.x, eachRect.y]}
+              fill="black"
+              stroke="black"
+              draggable={eachRect.draggable}
+              key={eachRect.x + eachRect.y}
+            />
+          ))}
+        </Layer>
+        <Layer>
+          <ToolBar />
+        </Layer>
+      </Stage>
+    </div>
   );
 };
 
