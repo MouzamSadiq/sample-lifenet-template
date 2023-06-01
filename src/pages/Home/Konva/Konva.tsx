e13c6864aa1dcdbd065a7245219ba34b2d58f70d
MouzamSadiq <mouzam@systalent>
2023-06-01 16:56:05 +0300

    added text tranform logic

--- src/pages/Home/Konva/Konva.tsx
+++ src/pages/Home/Konva/Konva.tsx
@@ -21,9 +21,8 @@ interface ShapeProps {
 interface TextProps {
   x: number;
   y: number;
-  fontSize: string;
+  fontSize: number;
   fontFamily: string;
-  ref: string;
   name: string;
   text: string;
   fill: string;
@@ -32,6 +31,7 @@ interface TextProps {
   rotation: number;
   textWidth: number;
   textHeight: number;
+  id: string;
 }
 
 const ArrowShape: React.FC<{
@@ -112,6 +112,76 @@ const ArrowShape: React.FC<{
   );
 };
 
+const TextShape: React.FC<{
+  shapeProps: TextProps;
+  isSelected: boolean;
+  onSelect: () => void;
+  onChange: (newAttrs: any) => void;
+}> = ({ shapeProps, isSelected, onSelect, onChange }) => {
+  const shapeRef = useRef<any>();
+  const trRef = useRef<any>();
+
+  useEffect(() => {
+    if (isSelected) {
+      // Attach transformer manually when selected
+      trRef.current.nodes([shapeRef.current]);
+      trRef.current.getLayer().batchDraw();
+    }
+  }, [isSelected]);
+
+  const cursorStyle = isSelected ? "move" : "pointer";
+
+  return (
+    <>
+      <Text
+        // stroke="black"
+        onClick={onSelect}
+        ref={shapeRef}
+        draggable
+        {...shapeProps}
+        onDragEnd={(e) => {
+          onChange({
+            ...shapeProps,
+            x: e.target.x(),
+            y: e.target.y(),
+          });
+        }}
+        onTransformEnd={(e) => {
+          const node = shapeRef.current;
+          const scaleX = node.scaleX();
+          const scaleY = node.scaleY();
+          node.scaleX(1);
+          node.scaleY(1);
+          onChange({
+            ...shapeProps,
+            x: node.x(),
+            y: node.y(),
+            fontSize: shapeProps.fontSize * Math.max(scaleX, scaleY), // Adjust the font size based on the scale
+          });
+        }}
+        onMouseEnter={() => {
+          shapeRef.current.getStage().container().style.cursor = cursorStyle;
+        }}
+        onMouseLeave={() => {
+          shapeRef.current.getStage().container().style.cursor = "default";
+        }}
+      />
+      {isSelected && (
+        <Transformer
+          ref={trRef}
+          boundBoxFunc={(oldBox: any, newBox: any) => {
+            // Limit resize
+            if (newBox.width < 5 || newBox.height < 5) {
+              return oldBox;
+            }
+            return newBox;
+          }}
+        />
+      )}
+    </>
+  );
+};
+
 const KonvaGround: React.FC = () => {
   // useEffect(() => {
   //   const handleResize = () => {
@@ -128,9 +198,11 @@ const KonvaGround: React.FC = () => {
   const [stageWidth, setStageWidth] = useState(window.innerWidth);
   const [stageHeight, setStageHeight] = useState(window.innerHeight);
   const [arrows, setArrows] = useState<ShapeProps[]>([]);
-  const [texts, setTexts] = useState<TextProps[]>([]);
-
   const [selectedId, selectShape] = useState<string | null>(null);
+
+  const [texts, setTexts] = useState<TextProps[]>([]);
+  const [selectedTextId, selectTextShape] = useState<string | null>(null);
+
   const toolbarArrowReference = useRef<any>(null);
   const toolbarDraggableTextRef = useRef<any>(null);
 
@@ -139,6 +211,7 @@ const KonvaGround: React.FC = () => {
     const clickedOnEmpty = e.target === e.target.getStage();
     if (clickedOnEmpty) {
       selectShape(null);
+      selectTextShape(null);
     }
   };
 
@@ -315,22 +388,24 @@ const KonvaGround: React.FC = () => {
   const handleTextDragEnd = () => {
     const draggableText = toolbarDraggableTextRef.current;
 
-    // const newArrow: ShapeProps = {
-    //   x: draggableArrow.getStage().getPointerPosition().x,
-    //   y: draggableArrow.getStage().getPointerPosition().y,
-    //   fill: "black",
-    //   draggable: true,
-    //   id: Math.random().toString(16).slice(2),
-    //   points: [0, 0, 0, 0],
-    // };
+    const newText: any = {
+      x: draggableText.getStage().getPointerPosition().x,
+      y: draggableText.getStage().getPointerPosition().y,
+      fontSize: 20,
+      fill: "black",
+      id: Math.random().toString(16).slice(2),
+      text: "15 cm",
+      // width: 150,
+      // height: 250,
+    };
 
     // Reset draggableText position
     draggableText.setAttrs({
-      x: 955,
-      y: 35,
+      x: 340,
+      y: 350,
     });
 
-    // setArrows([...arrows, newArrow]);
+    setTexts([...texts, newText]);
     // selectShape(newArrow.id);
   };
 
@@ -341,6 +416,7 @@ const KonvaGround: React.FC = () => {
       onMouseDown={checkDeselect}
       onTouchStart={checkDeselect}
     >
+      {/* <KonvaEditableText /> */}
       <Layer>
         <URLImage src="https://lifenethealth.visualstudio.com/8f99a695-9545-4cb5-a249-dbfe0d365a3f/_apis/git/repositories/92dd74a8-03df-4361-aaa0-32fe4091992f/items?path=/ProcessingLogs/ProcessingLogs_iOS/ProcessingLogs_Aortoilliac/Resources/AortoIliacArteryImage.gif&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=octetStream&api-version=5.0" />
       </Layer>
@@ -362,6 +438,42 @@ const KonvaGround: React.FC = () => {
               setArrows(updatedArrows);
             }}
           />
+        ))}
+        {texts.map((textProps, i) => (
+          <>
+            <TextShape
+              key={i}
+              shapeProps={textProps}
+              isSelected={textProps.id === selectedTextId}
+              onSelect={() => {
+                selectTextShape(textProps.id);
+              }}
+              onChange={(newAttrs) => {
+                const updatedShape = texts.slice();
+                updatedShape[i] = newAttrs;
+                setTexts(updatedShape);
+              }}
+            />
+            {/* <Text
+              key={i}
+              x={textProps.x}
+              y={textProps.y}
+              id={textProps.id}
+              // width={100}
+              // height={250}
+              text={textProps.text}
+              // shapeProps={arrow}
+              // isSelected={arrow.id === selectedId}
+              // onSelect={() => {
+              //   selectShape(arrow.id);
+              // }}
+              // onChange={(newAttrs) => {
+              //   const updatedArrows = arrows.slice();
+              //   updatedArrows[i] = newAttrs;
+              //   setArrows(updatedArrows);
+              // }}
+            /> */}
+          </>
         ))}
       </Layer>
     </Stage>
