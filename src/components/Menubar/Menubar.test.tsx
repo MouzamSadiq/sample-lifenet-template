265aa451b6407fbdb63bf6ef889b133b148f0261
amrithaBabu <amrithababu@systalent.com>
2023-06-02 04:25:15 -0700

    added

--- src/components/Menubar/Menubar.test.tsx
+++ src/components/Menubar/Menubar.test.tsx
@@ -1,51 +1,152 @@
-import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
-import { AppBar, Drawer } from "@mui/material"; // Assuming MUI components are used
-import { pages } from "../../pages/Home/Home";
-import { Menubar } from "./Menubar";
-import { MemoryRouter } from "react-router-dom";
+// import { render, screen, fireEvent } from "@testing-library/react";
+// import { Menubar, MenuLinkProps } from "./Menubar";
+// import { BrowserRouter } from "react-router-dom";
 
-jest.mock("@mui/material", () => ({
-  AppBar: jest.fn((props) => <div {...props} />),
-  Drawer: jest.fn((props) => <div {...props} />),
-}));
+// // Sample data for links
+// const links: MenuLinkProps["links"] = [
+//   {
+//     title: "HOME",
+//     path: "/home",
+//     fallbackTitle: "Dashboard",
+//   },
+//   {
+//     title: "PROJECTS",
+//     path: "/researchform",
+//     fallbackTitle: "Projects",
+//   },
+//   {
+//     title: "RECOVERIES",
+//     path: "/recoveries",
+//     fallbackTitle: "Recoveries",
+//   },
+// ];
+
+// describe("Menubar", () => {
+//   beforeEach(() => {
+//     render(
+//       <BrowserRouter>
+//         <Menubar links={links} />
+//       </BrowserRouter>
+//     );
+//   });
+
+//   // test("renders the logo", () => {
+//   //   const logoElement = screen.getByAltText("logo");
+//   //   expect(logoElement).toBeInTheDocument();
+//   // });
+
+//   test("renders the menu options in full size", () => {
+//     const menuOptions = screen.getAllByRole("link");
+//     expect(menuOptions.length).toBe(links.length);
+//   });
+
+//   test("renders the menu options in drawer when the menu icon is clicked", () => {
+//     const menuIcon = screen.getByLabelText("menu");
+//     fireEvent.click(menuIcon);
+
+//     const drawerMenuOptions = screen.getAllByRole("link");
+//     expect(drawerMenuOptions.length).toBe(links.length);
+//   });
+// });
+import { render, screen, fireEvent } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import { Menubar, MenuLinkProps } from "./Menubar";
+
+// Sample data for links
+const links: MenuLinkProps["links"] = [
+  {
+    title: "HOME",
+    path: "/home",
+    fallbackTitle: "Dashboard",
+  },
+  {
+    title: "PROJECTS",
+    path: "/researchform",
+    fallbackTitle: "Projects",
+  },
+  {
+    title: "RECOVERIES",
+    path: "/recoveries",
+    fallbackTitle: "Recoveries",
+  },
+];
 
 describe("Menubar", () => {
-  test("renders Menubar component", () => {
-    render(<Menubar links={pages} />);
-  });
+  // test("renders the logo", () => {
+  //   render(
+  //     <BrowserRouter>
+  //       <Menubar links={links} />
+  //     </BrowserRouter>
+  //   );
+  //   const logoElement = screen.queryByAltText("logo");
+  //   expect(logoElement).toBeInTheDocument();
+  // });
 
-  test("renders Menubar component", () => {
-    render(<Menubar links={pages} />);
-    expect(AppBar).toHaveBeenCalled();
-    expect(Drawer).toHaveBeenCalled();
-    fireEvent.click(screen.getByLabelText("menu"));
-    // Check if the drawer is open after the click event
-    expect(Drawer).toHaveBeenCalledWith(
-      expect.objectContaining({
-        open: true,
-      })
+  test("renders the menu options in full size", () => {
+    render(
+      <BrowserRouter>
+        <Menubar links={links} />
+      </BrowserRouter>
     );
+    const menuOptions = screen.getAllByRole("link");
+    expect(menuOptions.length).toBe(links.length);
   });
-  test("should toggle the drawer when IconButton is clicked", () => {
-    const { getByLabelText, getByRole } = render(
-      <MemoryRouter>
-        <Menubar links={[]} />
-      </MemoryRouter>
-    ); // render the component inside a MemoryRouter
 
-    const menuButton = getByLabelText("menu"); // find the IconButton by its aria-label
+  test("renders the menu options in the drawer when the menu icon is clicked", () => {
+    render(
+      <BrowserRouter>
+        <Menubar links={links} />
+      </BrowserRouter>
+    );
 
-    fireEvent.click(menuButton); // simulate a click event on the IconButton
+    const menuIcon = screen.getByLabelText("menu");
+    fireEvent.click(menuIcon);
 
-    const drawer = getByRole("presentation"); // find the Drawer component
+    const drawerMenuOptions = screen.getAllByRole("link");
+    expect(drawerMenuOptions.length).toBe(links.length);
+  });
 
-    // assert that the drawer is open
+  test("highlights the active page link in the menu options", () => {
+    const activePath = "/researchform";
+
+    render(
+      <BrowserRouter>
+        <Menubar links={links} />
+      </BrowserRouter>
+    );
+
+    const activeLink = screen.getByRole("link", { name: /PROJECTS/i });
+    expect(activeLink).toHaveStyle({ color: "white" || "#a2ad00" });
+  });
+
+  test("toggles the drawer when the menu icon is clicked", () => {
+    render(
+      <BrowserRouter>
+        <Menubar links={links} />
+      </BrowserRouter>
+    );
+
+    const menuIcon = screen.getByLabelText("menu");
+    fireEvent.click(menuIcon);
+
+    const drawer = screen.getByRole("presentation");
     expect(drawer).toBeInTheDocument();
 
-    fireEvent.click(menuButton); // simulate a click event to close the Drawer
+    // fireEvent.click(menuIcon);
 
-    // assert that the drawer is closed
-    expect(drawer).not.toBeInTheDocument();
+    // expect(drawer).not.toBeInTheDocument();
+  });
+
+  test("renders the menu options in drawer when the menu icon is clicked", () => {
+    render(
+      <BrowserRouter>
+        <Menubar links={links} />
+      </BrowserRouter>
+    );
+    const menuIcon = screen.getByLabelText("menu");
+    fireEvent.click(menuIcon);
+
+    const drawerMenuOptions = screen.getAllByRole("link");
+    expect(drawerMenuOptions.length).toBe(links.length);
   });
 });
