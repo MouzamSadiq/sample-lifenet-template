e72ce6dcb7fc47c3f930f9da829635c2741d3991
MouzamSadiq <mouzam@systalent>
2023-06-07 20:41:12 +0300

    Added types

--- src/pages/Home/Konva/types.ts
+++ src/pages/Home/Konva/types.ts
@@ -19,3 +19,40 @@ export interface ArrowShapeProps {
   id: string;
   points: number[];
 }
+
+export interface TextShapeProps {
+  x: number;
+  y: number;
+  fontSize: number;
+  fontFamily: string;
+  name: string;
+  // text: string;
+  fill: string;
+  width: number;
+  height: number;
+  rotation: number;
+  textWidth: number;
+  textHeight: number;
+  id: string;
+  customText: string;
+  draggable: boolean;
+  isFromTemplate: boolean;
+}
+
+export interface ShadingToolProps {
+  x: number;
+  y: number;
+  fill: string;
+  draggable?: boolean;
+  id: string;
+  width: number;
+  height: number;
+}
+export interface CustomArrowProps {
+  x: number;
+  y: number;
+  fill: string;
+  draggable: boolean;
+  id: string;
+  points: number[];
+}
