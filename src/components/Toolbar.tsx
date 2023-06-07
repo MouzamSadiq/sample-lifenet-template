afbda5b502b0fa72affa1b22468216497f55634b
MouzamSadiq <mouzam@systalent>
2023-06-07 22:41:42 +0300

    splitted toolbar

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,28 +1,36 @@
-import React from "react";
+import React, { RefObject } from "react";
 import { Arrow, Rect, Text } from "react-konva";
 
 interface ToolBarProps {
-  stageWidth: number;
   toolbarArrowReference: any;
-  toolbarDraggableTextRef: any;
-  customArrowReference?: any;
-  shadingToolRef?: any;
+  toolbarTextRef: any;
+  toolbarCustomArrowRef: any;
+  toolbarShadeRef: any;
   handleArrowDragEnds: () => void;
   handleTextDragEnd: () => void;
+  handleShadeDragEnd: () => void;
+  handleCustomShapeDragEnd: () => void;
+  toolbarX: number;
+  toolbarY: number;
+  toolbarHeight: number;
+  toolbarWidth: number;
 }
 
 const Toolbar: React.FC<ToolBarProps> = (props) => {
   const {
-    stageWidth,
     toolbarArrowReference,
-    toolbarDraggableTextRef,
+    toolbarTextRef,
+    toolbarCustomArrowRef,
+    toolbarShadeRef,
     handleArrowDragEnds,
     handleTextDragEnd,
+    handleShadeDragEnd,
+    handleCustomShapeDragEnd,
+    toolbarX,
+    toolbarY,
+    toolbarHeight,
+    toolbarWidth,
   } = props;
-  const rectWidth = 555;
-  const rectHeight = 77.5;
-  const rectX = (stageWidth - rectWidth) / 2;
-  const rectY = 5;
 
   const handleMouseEnter = () => {
     document.body.style.cursor = "pointer";
@@ -31,35 +39,33 @@ const Toolbar: React.FC<ToolBarProps> = (props) => {
   const handleMouseLeave = () => {
     document.body.style.cursor = "default";
   };
-  console.log("ReactX", rectX, "ReactY", rectY);
 
   return (
     <>
-      <Text
-        fontSize={30}
-        text="Toolbar"
-        fontFamily="Belgrano"
-        x={rectWidth}
-        y={rectHeight / 2}
-      />
       <Rect
-        x={rectX}
-        y={rectY}
-        width={555}
-        height={rectHeight}
+        x={toolbarX}
+        y={toolbarY}
+        width={700}
+        height={toolbarHeight}
         fill="white"
         shadowBlur={5}
         shadowColor="black"
+        cornerRadius={8}
       />
-      <Text text="Shape Arrow" fontSize={15} x={rectX + rectWidth / 6} y={10} />
+      <Text
+        text="Shape Arrow"
+        fontSize={15}
+        x={toolbarX + toolbarWidth / 6}
+        y={15}
+      />
 
       <Arrow
         id="toolbarArrow"
         points={[
-          rectX + rectWidth / 6,
-          rectY + rectHeight / 2,
-          rectX + rectWidth / 3,
-          rectY + rectHeight / 2,
+          toolbarX + toolbarWidth / 6,
+          toolbarY + toolbarHeight / 1.5,
+          toolbarX + toolbarWidth / 3,
+          toolbarY + toolbarHeight / 1.5,
         ]}
         fill="black"
         stroke="black"
@@ -70,49 +76,67 @@ const Toolbar: React.FC<ToolBarProps> = (props) => {
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
       />
-      <Text text="Text editor" fontSize={15} x={935} y={10} />
+      <Text
+        text="Text editor"
+        fontSize={15}
+        x={toolbarX + toolbarWidth / 2.2}
+        y={15}
+      />
       <Text
         fontSize={40}
         text="T"
         fontFamily="Belgrano"
-        x={rectX + rectWidth / 2}
-        y={rectY + rectHeight / 3}
+        x={toolbarX + toolbarWidth / 2}
+        y={toolbarY + toolbarHeight / 2.5}
         draggable
-        ref={toolbarDraggableTextRef}
+        ref={toolbarTextRef}
         onDragEnd={handleTextDragEnd}
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
       />
-      <Text text="Shade" fontSize={15} x={1025} y={10} />
+      <Text
+        text="Shade"
+        fontSize={15}
+        x={toolbarX + toolbarWidth / 1.4}
+        y={15}
+      />
       <Rect
-        x={rectX + rectWidth - 175}
-        y={rectY + rectHeight / 3}
+        x={toolbarX + toolbarWidth / 1.4}
+        y={toolbarY + toolbarHeight / 2.5}
         width={40}
-        height={40}
+        height={30}
         fill="transparent"
         stroke="black"
+        opacity={0.5}
         strokeWidth={1}
         shadowBlur={5}
+        draggable
+        ref={toolbarShadeRef}
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
-        // draggable
+        onDragEnd={handleShadeDragEnd}
       />
-      <Text text="Custom shape" fontSize={15} x={1125} y={10} />
+      <Text
+        text="Custom shape"
+        fontSize={15}
+        x={toolbarX + toolbarWidth / 1.1}
+        y={15}
+      />
       <Arrow
-        id="toolbarArrow"
+        id="customArrowShape"
         points={[
-          rectX + rectWidth - 105,
-          rectY + rectHeight - 30,
-          rectX + rectWidth - 40,
-          rectY + rectHeight - 30,
-          rectX + rectWidth - 20,
-          rectY + rectHeight / 3,
+          toolbarX + toolbarWidth - 30,
+          toolbarY + toolbarHeight - 20,
+          toolbarX + toolbarWidth + 30,
+          toolbarY + toolbarHeight - 20,
+          toolbarX + toolbarWidth + 50,
+          toolbarY + toolbarHeight / 2,
         ]}
         fill="black"
         stroke="black"
-        ref={toolbarArrowReference}
-        // draggable={true}
-        // onDragEnd={handleDragEnds}
+        draggable
+        ref={toolbarCustomArrowRef}
+        onDragEnd={handleCustomShapeDragEnd}
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
       />
