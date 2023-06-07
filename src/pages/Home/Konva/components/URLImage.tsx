83fe4cae1bf02e18227c0c6276afa84b6adf2883
MouzamSadiq <mouzam@systalent>
2023-06-07 22:41:29 +0300

    Slitted image rendering

---
+++ src/pages/Home/Konva/components/URLImage.tsx
@@ -0,0 +1,33 @@
+import { Image } from "react-konva";
+import React, { useEffect, useState } from "react";
+
+export const URLImage: React.FC<{ src: string }> = ({ src }) => {
+  // Calculate the position to center the image
+  const imageX = (window.innerWidth - 355) / 2;
+  const imageY = (window.innerHeight - 377.5) / 2; // 377.5 is from imageHeight -  toolbar bar height
+
+  const [image, setImage] = useState<HTMLImageElement>();
+  console.log("Image X and Y>><<<<<<", imageX, imageY);
+  useEffect(() => {
+    const img = new window.Image();
+    img.src = src;
+    img.onload = () => {
+      setImage(img);
+    };
+  }, [src]);
+
+  return (
+    <>
+      <Image
+        // onClick={() => {          // TODO: add onclick function to deSelect the shapes over image
+        //   selectShape(null);
+        // }}
+        image={image}
+        x={imageX}
+        y={imageY}
+        width={355}
+        height={455}
+      />
+    </>
+  );
+};
